2844320445be8627b7dcc4cd123d376905138d70
Giacomo Rebonato <grebonato@hubspot.com>
2018-09-06 14:03:29 -0400

    First commit

---
+++ src/components/Bio.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Kyle Mathews`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Written by <strong>Giacomo Rebonato</strong> who lives and works in Dublin building useful things.{' '}
+          <a href="https://twitter.com/kylemathews">
+            You should follow him on Twitter
+          </a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
